57a93c9623ac469063fdc0a1eb91ae652b1e001d
André Guimarães Aragon <andregaragon@gmail.com>
2024-11-01 07:19:32 -0300

    hello method added

--- lib/bucket_fishing.rb
+++ lib/bucket_fishing.rb
@@ -5,4 +5,9 @@ require_relative "bucket_fishing/version"
 module BucketFishing
   class Error < StandardError; end
   # Your code goes here...
+  module_function
+
+  def hello
+    "Hello, world!"
+  end
 end
